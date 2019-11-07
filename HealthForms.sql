31660b1ce3e3d25890de7b068b5e886515ed67de
jayboat23 <53226432+jayboat23@users.noreply.github.com>
2019-11-07 05:35:45 -0500

    Add files via upload

---
+++ HealthForms.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE HEALTHFORMS(
+Reason_of_Visit VARCHAR(1000),
+DATE_OF_VISIT TIMESTAMP,
+Health_Form_Number int,
+HealthForm_ID int ,
+Employee_ID int FOREIGN KEY REFERENCES HEALTHPROVIDERS(Employee_ID),
+Patient_ID int FOREIGN KEY REFERENCES Patients(Patient_ID),
+PRIMARY KEY CLUSTERED ([HealthForm_ID] Asc),
+);
